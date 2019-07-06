2ff1f978d1329488f68097c23dfba5a3efa2fe5a
zmxyt <812574754@qq.com>
2019-07-06 18:14:02 +0800

    signalmanage

---
+++ TutorSystem/src/signalmanageobject/signalmanageobject.h
@@ -0,0 +1,36 @@
+﻿#ifndef SIGNALMANAGEOBJECT_H
+#define SIGNALMANAGEOBJECT_H
+
+#include <QObject>
+#include <QWidget>
+
+
+class SignalManageObject : public QObject
+{
+    Q_OBJECT
+public:
+    explicit SignalManageObject(QObject *parent = 0);
+
+    void connectAllSignal();
+
+signals:
+
+public slots:
+    void setMainWidget(QWidget*);
+    void setLoginWidget(QWidget*);
+    void setTeacherWidget(QWidget*);
+    void setStudentWidget(QWidget*);
+    void setRegisterTeacherWidget(QWidget*);
+    void setRegisterStudentWidget(QWidget*);
+
+private:
+    QWidget *m_MainWidget = NULL;
+    QWidget *m_LoginWidget = NULL;
+    QWidget *m_TeacherWidget = NULL;
+    QWidget *m_StudentWidget = NULL;
+    QWidget *m_RegisterTeacherWidget = NULL;
+    QWidget *m_RegisterStudentWidget = NULL;
+
+};
+
+#endif // SIGNALMANAGEOBJECT_H
