a0ebbd0bc48cff5ff453710248990f17795a7730
yangzhe <812574754@qq.com>
2019-09-24 08:35:54 +0800

    user

---
+++ yz/userdatabase.cpp
@@ -0,0 +1,71 @@
+﻿#pragma execution_character_set("utf-8")
+/*******************************************
+* @className
+* @brief         Data Base
+*                v1.0
+* @author        cql
+* @date          2019-01-17
+********************************************/
+
+#include "userdatabase.h"
+#include <QDebug>
+#define DATA_BASE_NAME ("tutorsystem.db")//数据库文件名
+QSqlDatabase userDataBasePri::database;
+userDataBasePri::userDataBasePri()
+{
+    if (QSqlDatabase::contains("cql_sqllite")) {//连接数据库
+        database = QSqlDatabase::database("cql_sqllite");
+    } else {
+        database = QSqlDatabase::addDatabase("QSQLITE","cql_sqllite");
+    }
+    database.setDatabaseName(DATA_BASE_NAME);//设置数据库文件名
+    database.setUserName("root");//用户
+    database.setPassword("nopassword");//密码-无密码
+
+    if (! database.open()){//打开数据库
+        qCritical() << database.lastError().text();
+        return;
+    }
+
+    QSqlQuery query(database);
+
+    //教师
+    bool ret = query.exec(QString("create table teacher_information(teacherid varchar(20) primary key,teacher_password varchar(20),"
+                                  "teacher_name varchar(50),teacher_sex varchar(10),teacher_age varchar(10),teacher_phone varchar(20),"
+                                  "teacher_email varchar(20),teacher_level varchar(20),teacher_info varchar(200))"));
+
+    ret = query.exec(QString("create table need_information(needid varchar(50) primary key,need_name varchar(50),course varchar(50),start_time varchar(50),stop_time varchar(50),cost varchar(50),needgrade varchar(50),needstr varchar(200),issuer varchar(50),acceptissuer varchar(50))"));
+
+    QString insertser = QString("insert into student_information values('%1','%2','%3','%4','%5','%6','%7','%8')");
+    query.exec(QString("insert into teacher_information values('0001','passwd','张三','男','11','15566566556',"
+                       "'44444444@qq.com','小学','高级教师')"));
+    query.exec(QString("insert into teacher_information values('0002','passwd','张四','男','11','15566566556',"
+                       "'44444444@qq.com','初中','高级教师')"));
+    query.exec(QString("insert into teacher_information values('0003','passwd','张五','男','11','15566566556',"
+                       "'44444444@qq.com','高中','高级教师')"));
+    //教师评价
+    ret = query.exec(QString("create table %1(evaluateid varchar(20) primary key,evaluate_student varchar(20),evaluate_text varchar(100))").arg(QString("evaluate_0001")));
+    ret = query.exec(QString("insert into %1 values('0001','张三','666')").arg(QString("evaluate_0001")));
+    //教师评价
+    ret = query.exec(QString("create table %1(evaluateid varchar(20) primary key,evaluate_student varchar(20),evaluate_text varchar(100))").arg(QString("evaluate_0002")));
+    ret = query.exec(QString("insert into %1 values('0001','张三','666')").arg(QString("evaluate_0002")));
+    //教师评价
+    ret = query.exec(QString("create table %1(evaluateid varchar(20) primary key,evaluate_student varchar(20),evaluate_text varchar(100))").arg(QString("evaluate_0003")));
+    ret = query.exec(QString("insert into %1 values('0001','张三','666')").arg(QString("evaluate_0003")));
+
+
+    //学生
+    ret = query.exec(QString("create table student_information(studentid varchar(20) primary key,student_password varchar(20),"
+                             "student_name varchar(50),student_sex varchar(10),student_age varchar(10),student_phone varchar(20),"
+                             "student_email varchar(20),student_class varchar(20))"));
+    query.exec(QString("insert into student_information values('0001','passwd','张六','男','11','15566566556',"
+                       "'44444444@qq.com','5年级')"));
+    database.close();
+
+    qDebug()<<"ddddddddd"<<ret;
+}
+
+userDataBasePri::~userDataBasePri()
+{
+
+}
